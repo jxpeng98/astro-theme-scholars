063e4db916d376e78a5db2fe1a4d87bae0b57d7b
Jiaxin Peng <jiaxin.peng@outlook.com>
2025-10-23 23:14:50 +0100

    feat: Add sitemap dependency and create robots.txt for SEO optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://astro-theme-scholars.pages.dev/', // Your production site URL here
 	integrations: [icon()],
 	vite: {
 		plugins: [UnoCSS()]
