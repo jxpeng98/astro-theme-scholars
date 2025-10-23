e0d8ce6da86dc53e2a608c5aae0fa249baebb5de
Jiaxin Peng <jiaxin.peng@outlook.com>
2025-10-23 23:25:48 +0100

    feat: Add sitemap integration and update robots.txt for improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/vite';
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astro-theme-scholars.pages.dev/', // Your production site URL here
-	integrations: [icon()],
+	integrations: [icon(), sitemap()],
 	vite: {
 		plugins: [UnoCSS()]
 	}
